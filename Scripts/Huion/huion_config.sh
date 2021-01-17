73b935570dc1a41ebb61ca1fd6940c70bedc86d3
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2021-01-17 12:49:21 +0100

    added huion tablet config

---
+++ Scripts/Huion/huion_config.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+xsetwacom set "HUION Huion Tablet_H950P Pen stylus" MapToOutput eDP1
+
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 8 key "down down down down down down down down down down"
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 3 key "right right right right right right right right right right"
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 2 key "left left left left left left left left left left"
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 1 key "up up up up up up up up up up"
+
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 9 key "Prior"
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 10 key "Next"
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 11 key "+ctrl plus plus plus plus plus -ctrl "
+xsetwacom set "HUION Huion Tablet_H950P Pad pad" Button 12 key "+ctrl minus minus minus minus minus -ctrl "
+      
