1702f977c297d910eb995d8b0b76d0576daaf4ac
jensjpedersen <jens.junker.pedersen@gmail.com>
2021-02-09 18:28:33 +0100

    dynamic battery bar

--- .config/i3/Scripts/battery_bar.sh
+++ .config/i3/Scripts/battery_bar.sh
@@ -1,7 +1,30 @@
 #!/bin/zsh
 
-bat=$(acpi -b | awk -F "," '{print $2}')
-echo ${bat}
+bat=$(acpi -b | awk -F " |%" '{print $4}')
+charge=$(acpi -a | awk '{print $3}')
+
+if [ ${charge}="on-line" ]
+then
+    if [ ${bat} -lt 30 ]
+    then
+        echo  ${bat}%
+    elif [ ${bat} -gt 30 ] && [ ${bat} -lt 90 ]
+    then
+        echo  ${bat}%
+    else
+        echo  ${bat}%
+    fi
+else 
+    if [ ${bat} -lt 30 ]
+    then
+        echo  ${bat}%
+    elif [ ${bat} -gt 30 ] && [ ${bat} -lt 90 ]
+    then
+        echo  ${bat}%
+    else
+        echo  ${bat}%
+    fi
+fi
 #BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
 #BAT=$(acpi -b | awk)
 #echo "$BAT"
