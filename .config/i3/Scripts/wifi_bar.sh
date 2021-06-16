f23f63d4c067a4691375b8ade0846b8dac485048
jensjpedersen <jens.junker.pedersen@gmail.com>
2021-06-16 12:05:11 +0200

    fix

--- .config/i3/Scripts/wifi_bar.sh
+++ .config/i3/Scripts/wifi_bar.sh
@@ -1,4 +1,5 @@
 #! /bin/zsh
-
-signal=$(nmcli device wifi list | grep "*" | awk '{print $8}')%
+#signal=$(nmcli device wifi list | grep "*" | awk '{print $8}')%
+signal=$(nmcli device wifi list | awk '/*/ {print $(NF-2)}')%
 echo $signal
+
